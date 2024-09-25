1b15c0be860336e836ed897a90e585ae63ef1912
Erroraeto <erroraeto@gmail.com>
2024-09-26 02:21:17 +0300

    Correct pentagone & sizing

--- js/scroll-doc.js
+++ js/scroll-doc.js
@@ -1,21 +1,31 @@
 let content = document.querySelector('.content');
 let skills = document.querySelector('.skills');
 let about = document.querySelector('.about');
-let wrapper = document.querySelector('.cntntWrp');
+let frame = 'about';
 
 document.addEventListener('click', function (e) {
     if (!e.target.closest('.button')) return;
     
     let winSize = window.innerWidth;
-    content.style.transformOrigin = window.innerWidth / 2 + 'px';
 
-    if (e.target.closest('#aboutMe')) {
-        content.style.translate = '';
-        content.style.scale = '80%';
-    } else if (e.target.closest('#skill')) {
-        content.style.translate = -e.currentTarget.documentElement.offsetWidth + 'px';
-        content.style.scale = '80%';
-    };
+        if (e.target.closest('#aboutMe') && frame != 'about') {
+            skills.style.scale = '0.8';
+            about.style.scale = '0.8';
+            setTimeout(() => {
+                content.style.translate = '';
+                frame = 'about';
+            }, 300);
+        } else if (e.target.closest('#skill') && frame != 'skills') {
+            skills.style.scale = '0.8';
+            about.style.scale = '0.8';
+            setTimeout(() => {
+                content.style.translate = -winSize + 'px';
+                frame = 'skills';
+            }, 300);
+        };
 
-    setTimeout(() => content.style.scale = '', 500)
+    setTimeout(() => {
+        skills.style.scale = '';
+        about.style.scale = '';
+    }, 800)
 });
